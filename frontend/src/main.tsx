2e87383fa4d7a63887bebae0001ce4a3e3e817b1
Junaid Atari <mj.atari@gmail.com>
2025-12-02 15:17:59 +0500

    feat: wrap App with Theme component and add styles
    Add Theme component from @radix-ui/themes
    Wrap App component with Theme for styling
    Import Radix UI themes styles
    Update main.tsx to use new styling structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @copyright 2025 Junaid Atari
+ * @see https://github.com/blacksmoke26
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+
+import {Theme} from '@radix-ui/themes';
+
+// components
+import {App} from './App';
+
+// types
+import '@radix-ui/themes/styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Theme>
+      <App/>
+    </Theme>
+  </React.StrictMode>,
 );
