3de9734ca732414898d6f247e331b3a550d4bd15
Junaid Atari <mj.atari@gmail.com>
2026-02-07 00:02:01 +0500

    feat: add new Composer component
    Add a new React component file as a starting point for implementing a composer interface. The component currently renders a placeholder heading and will be expanded with functionality in future commits. No breaking changes as this is a new file.
    
    - Creates a reusable Composer component structure
    - Uses TypeScript for type safety
    - Follows standard React component naming conventions
    - Provides a foundation for future implementation

---
+++ frontend/src/components/Composer/index.tsx
@@ -0,0 +1,18 @@
+/**
+ * @author Junaid Atari <mj.atari@gmail.com>
+ * @copyright 2025 Junaid Atari
+ * @see https://github.com/blacksmoke26
+ */
+
+import React from 'react';
+
+export interface ComposerProps {
+}
+
+const Composer: React.FC<ComposerProps> = () => {
+  return (
+    <h3>Add contents here</h3>
+  );
+};
+
+export default Composer;
